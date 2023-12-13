9eec74d6305eb9417bfe1c43293eebebc0aea86e
Sevan <Sevan@INBOOK_X2_GEN11>
2023-12-13 20:50:57 +0700

    Добавьте файлы проекта.

---
+++ лаба 8 задание 1/лаба 8 задание 1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+
+using namespace std;
+
+/*
+* Лабораторная работа 8. Поливцев Михаил, ИВТ-31. Задание 1. Индивидуальный вариант задания 16.
+* Дан символ С и строка S. Сформировать из строки S строку S1,
+* в которой после каждого вхождения С вставить цифру 0.
+*/
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	string s, s1;
+	char c;
+
+	cout << "Введите строку S >> ";
+	getline(cin, s);
+	cout << "Введите символ С >> ";
+	cin >> c;
+
+	s1 = s;
+	int j = 1;
+	for (int i = 0; i < size(s); i++) {
+		if (s[i] == c) {
+			s1.insert(i + j, "0");
+			j++;
+		}
+	}
+
+	cout << s1 << endl;
+}
